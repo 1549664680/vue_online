8252a423cfdece9f7340043744bc55f792b12ffc
1549664680 <1549664680@qq.com>
2020-02-27 22:01:30 +0800

    alertshow

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,41 @@
     <!-- <router-view/> -->
     <!-- <goodsDetail/> -->
     <!-- <search/> -->
-    <router-view/>
+    <!-- <router-view/> -->
+    <alertTip v-if="showAlert" :alertText="alertText" @closeTip="closeAlert"/>
+    <Button @click="showCancelAlert">提示退出</Button>
+    <Button @click="showLoginAlert">提示登录</Button> 
   </div>
 </template>
 <script>
 import register from '@/components/register.vue'
 import goodsDetail from '@/components/goodsDetail.vue'
 import search from '@/components/search.vue'
+import alertTip from '@/components/common/alertTip.vue'
 export default {
-  components:{
-    register,
-    goodsDetail,
-    search
-  },
   data() {
     return {
-      
+      showAlert: false,
+      alertText: ''
     }
   },
   methods: {
-   
-}
+    closeAlert () {
+      this.showAlert = false
+    },
+    showCancelAlert() {
+      this.alertText = '您真的要退出吗?'
+      this.showAlert = true
+    },
+    showLoginAlert() {
+      this.alertText = '您真的要登录吗'
+      this.showAlert = true
+    }
+  },
+  components: {
+    alertTip,
+    // 怎么解决？
+  }  
 }
 </script>
 <style>
